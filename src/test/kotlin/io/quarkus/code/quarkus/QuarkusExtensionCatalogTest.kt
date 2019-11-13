8275c471d2e687b3bad3a6a9b8ac51f0128c0b2a
Andy Damevin <ia3andy@gmail.com>
2019-11-13 15:52:41 +0100

    Optimize imports

--- src/test/kotlin/io/quarkus/code/quarkus/QuarkusExtensionCatalogTest.kt
+++ src/test/kotlin/io/quarkus/code/quarkus/QuarkusExtensionCatalogTest.kt
@@ -1,10 +1,14 @@
 package io.quarkus.code.quarkus
 
-import io.quarkus.code.quarkus.model.CodeQuarkusExtension
 import io.quarkus.test.junit.QuarkusTest
+import io.quarkus.code.quarkus.model.CodeQuarkusExtension
 import org.hamcrest.MatcherAssert.assertThat
-import org.hamcrest.Matchers.*
+import org.hamcrest.Matchers.`is`
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Test
+import java.nio.file.Path
+import java.util.function.Function
 
 @QuarkusTest
 internal class QuarkusExtensionCatalogTest {
